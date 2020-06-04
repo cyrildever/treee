757b14d8a34d2408e693a5e87e37e675d66695f4
Cyril Dever <cdever@edgewhere.fr>
2020-06-04 17:30:10 +0200

    branch type and printable interface

---
+++ core/index/branch/branch.go
@@ -0,0 +1,72 @@
+package branch
+
+//--- TYPES
+
+// Branch ...
+type Branch struct {
+	nature interface{}
+}
+
+// Printable ...
+type Printable interface {
+	Print() string
+}
+
+//--- METHODS
+
+// Assign ...
+func (b *Branch) Assign(leafOrNode interface{}) {
+	b.nature = leafOrNode
+}
+
+// GetLeaf ...
+func (b *Branch) GetLeaf() *Leaf {
+	if l, ok := b.nature.(Leaf); ok {
+		return &l
+	}
+	return &Leaf{}
+}
+
+// GetNode ...
+func (b *Branch) GetNode() *Node {
+	if n, ok := b.nature.(Node); ok {
+		return &n
+	}
+	return &Node{}
+}
+
+// IsEmpty ...
+func (b *Branch) IsEmpty() bool {
+	return b.nature == nil || (!b.IsLeaf() && !b.IsNode())
+}
+
+// IsLeaf ...
+func (b *Branch) IsLeaf() bool {
+	nature := b.nature
+	if _, ok := nature.(Leaf); ok {
+		return true
+	}
+	return false
+}
+
+// IsNode ...
+func (b *Branch) IsNode() bool {
+	nature := b.nature
+	if _, ok := nature.(Node); ok {
+		return true
+	}
+	return false
+}
+
+// Print ...
+func (b *Branch) Print() string {
+	if b.IsLeaf() {
+		l, _ := b.nature.(Leaf)
+		return l.Print()
+	} else if b.IsNode() {
+		n, _ := b.nature.(Node)
+		return n.Print()
+	} else {
+		return "{}"
+	}
+}
